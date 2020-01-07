de95270fbc3f24ee5b74c0d83d5d10d7cd7ae497
Camha <crystalhanguyen@gmail.com>
2020-01-07 19:10:02 +0000

    create 404 pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,15 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
 
