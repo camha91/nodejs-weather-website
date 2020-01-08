24c751dac4f7244d7c48f449bc5f6414d397fb99
Camha <crystalhanguyen@gmail.com>
2020-01-08 22:04:38 +0000

    User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,31 @@
 console.log("Client side js file is loaded!")
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListeners('submit', (e) => {
+	
+	e.preventDefault()
+	
+	const location = search.value
+	
+	messageOne.textContent = 'Loading...'
+	messageTwo.textContent = ''
+	
+	fetch(`54.180.97.56:56419/weather?address=${location}`).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				messageOne.textContent = data.error
+			} else {
+				messageOne.textContent = data.location
+				messageTwo.textContent = data.forecast
+			}
+		})
+	})
+	
+})
