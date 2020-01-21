95d1cfe032ab2ee52061a49a10c429a39611656a
camha91 <crystalhanguyen@gmail.com>
2020-01-20 22:00:23 -0800

    Resourse reading endpoints using app.get

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,6 +18,31 @@ app.post('/users', (req, res) => {
    })
 })
 
+
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+
 app.post('/tasks', (req, res) => {
     const task = new Tasks(req.body)
 
@@ -27,6 +52,29 @@ app.post('/tasks', (req, res) => {
         res.status(400).send(e)
     })
 })
+
+app.get('/tasks', (req, res) => {
+    Tasks.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Tasks.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
