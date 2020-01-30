f86341c864be92c6fb8de7cf4d1b34addda93da6
Camha Nguyen <crystalhanguyen@gmail.com>
2020-01-30 18:03:05 +0000

    install bcrypt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -10,6 +10,21 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+const bcrypt = require('bcrypt')
+
+const myFunction = async () => {
+	const password = 'Green12345!'
+	const hashedPassword = await bcrypt.hash(password, 8)
+	
+	console.log(hashedPassword)
+	
+	const isMatch = await bcrypt.compare('Green12345!', hashedPassword)
+	console.log(isMatch)
+}
+
+myFunction()
+
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
